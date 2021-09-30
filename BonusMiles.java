a3e8ca604e6b69159333a3c6695a9fa013400594
Aleksandra <89194443+shadrinova@users.noreply.github.com>
2021-09-30 14:34:41 +0300

    Create BonusMiles.java

---
+++ BonusMiles.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        long amount = 10_000;
+        int percent = 20;
+
+        long bonus = amount / percent;
+        System.out.println("Зачислено " + bonus + " миль");
+
+    }
+}
